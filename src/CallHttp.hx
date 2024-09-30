f049050578ebe63f938420401efb7ca8e0b6c8bc
Josu Igoa <josuigoa@gmail.com>
2024-09-30 11:26:42 +0200

    create CustomHttp while Haxe stdlib is not ready

--- src/CallHttp.hx
+++ src/CallHttp.hx
@@ -1,7 +1,8 @@
 class CallHttp {
 	public static function callEndpoint(endpoint:String, post:Bool = false) {
 		return new js.lib.Promise((resolve, reject) -> {
-			var http = new haxe.Http(endpoint);
+			// use CustomHttp while some changes are not not in Haxe's std library
+			var http = new CustomHttp(endpoint);
 			http.addHeader('Accept', 'application/json');
 			http.onError = reject;
 			http.onBytes = resolve;
@@ -11,7 +12,8 @@ class CallHttp {
 
 	public static function ping(port:Int) {
 		return new js.lib.Promise<Int>((resolve, reject) -> {
-			var http = new haxe.Http('http://0.0.0.0:${port}/ping');
+			// use CustomHttp while some changes are not not in Haxe's std library
+			var http = new CustomHttp('http://0.0.0.0:${port}/ping');
 			http.onError = reject;
 			http.onData = d -> if (d == 'pong') resolve(port) else resolve(-1);
 			http.request();
